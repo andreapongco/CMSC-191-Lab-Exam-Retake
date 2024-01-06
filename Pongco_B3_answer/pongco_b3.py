3c5c7b77c09905729f6fdd5ae30feb3dfe25423d
andreapongco <69851785+andreapongco@users.noreply.github.com>
2024-01-06 20:26:05 +0800

    Add files via upload

--- Pongco_B3_answer/pongco_b3.py
+++ Pongco_B3_answer/pongco_b3.py
@@ -12,7 +12,7 @@ with open("../input_file/Exam_Table.csv", "r") as input_file:
 
     # open a new csv file as output and only write the rows with Genus starting with "St", case insensitive
     with open("../Pongco_B3_answer/b3_output1.csv", "w", newline='') as output_file:
-        writer = csv.DictWriter(output_file, fieldnames=['Scientific Name'], delimiter=",")
+        writer = csv.DictWriter(output_file, fieldnames=['Scientific Name',], delimiter=",")
         writer.writeheader()
 
         unique_scientific_names = []
